f03fdf26ce2176eb178c4aa2df27659f274f2fec
marimo123-sudo <galievr323@gmail.com>
2024-08-06 10:30:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     Telegram.WebApp.ready();
-    Telegram.WebApp.webAppVersion = 7.8;
     Telegram.WebApp.setHeaderColor('#000000');
     Telegram.WebApp.disableClosingConfirmation();
     Telegram.WebApp.enableVerticalSwipes();
 });
+
+// Предотвращаем зум на iOS при двойном тапе
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function(event) {
+  let now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
