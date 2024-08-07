77d2dfa5bdf05b25f1ca72ac2be7654bbb004ba4
marimo123-sudo <galievr323@gmail.com>
2024-08-07 12:58:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ document.addEventListener('touchmove', function (event) {
     event.preventDefault();
   }
 }, { passive: false });
+
+function updateObjectPositions() {
+    var viewport_height = Telegram.WebApp.viewportHeight;
+    console.log(viewport_height);
+}
+
+window.addEventListener('resize', updateObjectPositions);
+
+var viewport_height = Telegram.WebApp.viewportHeight;
+
+console.log(viewport_height);
+
+document.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+}, { passive: false });
+
+document.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('gesturestart', function(event) {
+    event.preventDefault();
+});
