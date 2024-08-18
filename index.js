c1717a63fb8fc2bcd8784f596b3152bb93c906ce
marimo123-sudo <galievr323@gmail.com>
2024-08-18 22:24:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+var contin = document.getElementById("btn");
+var visible_object1 = document.getElementById("visibility");
+var images = document.getElementById("images")
+var register = document.getElementById("reg")
+var go_btn = document.getElementById("go")
+var question = document.getElementById("question")
+var hi_free = document.getElementById("free")
+var count = 0
 
+
+contin.addEventListener('click', function() {
+    if (count == 0) {
+        visible_object1.style.display = 'none';
+        images.classList.add('move-up'); // Добавляем класс для перемещения элемента
+        register.classList.add("visible"); //делаем объект регитсрации видимым
+        count = count + 1;
+    }
+});
+
+
+go_btn.addEventListener('click', function() {
+    images.classList.add("move-up2");
+    register.classList.add("move-up2");
+    question.style.display = 'none';
+    hi_free.classList.add('visible')
+});
+    
