3f306d8284ee0e55ccb89917359a214113ede003
marimo123-sudo <galievr323@gmail.com>
2024-08-18 23:30:33 +0300

    Update anti_size.js

--- anti_size.js
+++ anti_size.js
@@ -1,11 +1,18 @@
 // Предотвращает масштабирование через жесты pinch (щипок)
+document.addEventListener('gesturestart', function(event) {
+  event.preventDefault(); // Запрещает изменение масштаба
+});
+
+// Предотвращает изменение масштаба через жесты pinch (щипок)
 document.addEventListener('touchmove', function(event) {
-  if (event.scale !== 1) {
-    event.preventDefault(); // Запрещает изменение масштаба
+  if (event.scale !== undefined && event.scale !== 1) {
+    event.preventDefault(); // Запрещает масштабирование
   }
 }, { passive: false });
 
-// Предотвращает масштабирование через жесты pinch (щипок) на устройствах с сенсорным экраном
-document.addEventListener('gesturestart', function(event) {
-  event.preventDefault(); // Запрещает масштабирование
-});
+function updateObjectPositions() {
+    var viewport_height = Telegram.WebApp.viewportHeight;
+    console.log(viewport_height);
+}
+
+window.addEventListener('resize', updateObjectPositions);
