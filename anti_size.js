357ef60f81691adebb9ca5dd6e8ab598190e5ce9
marimo123-sudo <galievr323@gmail.com>
2024-08-18 23:26:41 +0300

    Update anti_size.js

--- anti_size.js
+++ anti_size.js
@@ -1,32 +1,11 @@
-// Предотвращает масштабирование через жесты на устройствах с сенсорным экраном
-// document.addEventListener('gesturestart', function (e) {
-//     e.preventDefault();
-//   });
-  
-  // Запрещает изменение масштаба через жесты pinch (щипок)
-  document.addEventListener('touchmove', function (event) {
-    if (event.scale !== 1) {
-      event.preventDefault();
-    }
-  }, { passive: false });
-  // function updateObjectPositions() {
-  //     var viewport_height = Telegram.WebApp.viewportHeight;
-  //     console.log(viewport_height);
-  // }
-  
-  // window.addEventListener('resize', updateObjectPositions);
+// Предотвращает масштабирование через жесты pinch (щипок)
+document.addEventListener('touchmove', function(event) {
+  if (event.scale !== 1) {
+    event.preventDefault(); // Запрещает изменение масштаба
+  }
+}, { passive: false });
 
-
-//   document.addEventListener('touchmove', function(event) {
-//     event.preventDefault();
-// }, { passive: false });
-
-// document.addEventListener('touchstart', function(event) {
-//     if (event.touches.length > 1) {
-//         event.preventDefault();
-//     }
-// }, { passive: false });
-
-// document.addEventListener('gesturestart', function(event) {
-//     event.preventDefault();
-// });
+// Предотвращает масштабирование через жесты pinch (щипок) на устройствах с сенсорным экраном
+document.addEventListener('gesturestart', function(event) {
+  event.preventDefault(); // Запрещает масштабирование
+});
