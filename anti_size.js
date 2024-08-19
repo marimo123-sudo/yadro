dd9eb909500ca5d060bab493fc8376a8148e20c4
marimo123-sudo <galievr323@gmail.com>
2024-08-19 09:54:00 +0300

    Update anti_size.js

--- anti_size.js
+++ anti_size.js
@@ -31,10 +31,3 @@
 //     event.preventDefault();
 // });
 
-function setVh() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-window.addEventListener('resize', setVh);
-window.addEventListener('load', setVh);
