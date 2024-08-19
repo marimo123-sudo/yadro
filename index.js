dc03fd159f07e2a5ff4c530a0151099037d1a5d8
marimo123-sudo <galievr323@gmail.com>
2024-08-19 14:26:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var background = document.getElementById("back");
 var contin = document.getElementById("btn");
 var visible_object1 = document.getElementById("visibility");
 var images = document.getElementById("images")
@@ -6,6 +7,7 @@ var go_btn = document.getElementById("go")
 var question = document.getElementById("question")
 var hi_free = document.getElementById("free")
 var count = 0
+
 
 
 contin.addEventListener('click', function() {
@@ -28,5 +30,33 @@ go_btn.addEventListener('click', function() {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     Telegram.WebApp.setHeaderColor('#000000');
-    Telegram.WebApp.setBackgroundColor('#000000')
+    Telegram.WebApp.setBackgroundColor('#000000');
+    const heit = window.innerHeight;
+    console.log(heit);
+    
+    document.documentElement.style.setProperty('--viewport-height', `${heit}px`);
+});
+
+
+function isMobile() {
+    return /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    console.log('На мобильном устройстве');
+} else {
+    console.log('На ПК');
+}
+
+const inputElements = document.querySelectorAll('input, textarea');
+inputElements.forEach(input => {
+    input.addEventListener('focus', () => {
+        console.log('Input focused');
+        background.classList.add("upper");
+    });
+
+    input.addEventListener('blur', () => {
+        console.log('Input blurred');
+        background.classList.remove("upper");
+    });
 });
