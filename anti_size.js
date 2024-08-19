818e02defdf8786fcdec920b1a2a0fc8fb412684
marimo123-sudo <galievr323@gmail.com>
2024-08-19 09:31:05 +0300

    Update anti_size.js

--- anti_size.js
+++ anti_size.js
@@ -1,18 +1,49 @@
-// Предотвращает масштабирование через жесты pinch (щипок)
-document.addEventListener('gesturestart', function(event) {
-  event.preventDefault(); // Запрещает изменение масштаба
-});
+// Предотвращает масштабирование через жесты на устройствах с сенсорным экраном
+// document.addEventListener('gesturestart', function (e) {
+//     e.preventDefault();
+//   });
+  
+  // Запрещает изменение масштаба через жесты pinch (щипок)
+  document.addEventListener('touchmove', function (event) {
+    if (event.scale !== 1) {
+      event.preventDefault();
+    }
+  }, { passive: false });
+  // function updateObjectPositions() {
+  //     var viewport_height = Telegram.WebApp.viewportHeight;
+  //     console.log(viewport_height);
+  // }
+  
+  // window.addEventListener('resize', updateObjectPositions);
 
-// Предотвращает изменение масштаба через жесты pinch (щипок)
-document.addEventListener('touchmove', function(event) {
-  if (event.scale !== undefined && event.scale !== 1) {
-    event.preventDefault(); // Запрещает масштабирование
+
+//   document.addEventListener('touchmove', function(event) {
+//     event.preventDefault();
+// }, { passive: false });
+
+// document.addEventListener('touchstart', function(event) {
+//     if (event.touches.length > 1) {
+//         event.preventDefault();
+//     }
+// }, { passive: false });
+
+// document.addEventListener('gesturestart', function(event) {
+//     event.preventDefault();
+// });
+
+// anti_size.js
+function adjustElementsForKeyboard() {
+  const imagesElement = document.getElementById('images');
+  const registerElement = document.getElementById('reg');
+
+  if (window.innerHeight < 600) { // условное значение, которое нужно адаптировать под ваши нужды
+      imagesElement.classList.add('move-up');
+      registerElement.classList.add('move-up2');
+  } else {
+      imagesElement.classList.remove('move-up');
+      registerElement.classList.remove('move-up2');
   }
-}, { passive: false });
-
-function updateObjectPositions() {
-    var viewport_height = Telegram.WebApp.viewportHeight;
-    console.log(viewport_height);
 }
 
-window.addEventListener('resize', updateObjectPositions);
+window.addEventListener('resize', adjustElementsForKeyboard);
+window.addEventListener('load', adjustElementsForKeyboard);
