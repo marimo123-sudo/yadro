6522325a756981d8c636be6d346f0f3019cdfedb
marimo123-sudo <galievr323@gmail.com>
2024-08-20 20:58:20 +0300

    Delete main directory

--- main/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const heit = window.innerHeight;
-    console.log(heit);
-    Telegram.WebApp.setHeaderColor('#1a1a1a');
-    Telegram.WebApp.setBackgroundColor("#1a1a1a");
-    document.documentElement.style.setProperty('--viewport-height', `${heit}px`);
-});
