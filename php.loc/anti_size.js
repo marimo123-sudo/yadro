1cdac9256f87753847d46ce0fa6846b3874623d0
marimo123-sudo <galievr323@gmail.com>
2024-08-20 21:02:19 +0300

    Delete php.loc directory

--- php.loc/anti_size.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener('touchmove', function (event) {
-//     if (event.scale !== 1) {
-//       event.preventDefault();
-//     }
-//   }, { passive: false });
-
--- php.loc/main/main.js
+++
@@ -1,9 +0,0 @@
-Telegram.WebApp.setHeaderColor('#1a1a1a');
-Telegram.WebApp.setBackgroundColor("#1a1a1a");
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const heit = window.innerHeight;
-    console.log(heit);
-    document.documentElement.style.setProperty('--viewport-height', `${heit}px`);
-});
